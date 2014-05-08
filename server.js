a538e05c7f40b79889de7043dcecefae5c0d56f8
Lucas Cotner <lc07@uw.edu>
2014-05-08 14:15:35 -0700

    Adding files to get the Acceptance Testing via Casper going, in the new branch, casperTest

--- server.js
+++ server.js
@@ -10,4 +10,9 @@ var server = http.createServer(app);
 app.set('port', process.env.PORT || 3000);
 server.listen(app.get('port'), function() {
   console.log('the server is running on port ', app.get('port'));
+  //document.write("Bangg!");
+  //document.write(app.get('port'));
+});
+app.get('/', function(req, res){
+  res.send('hello world!');
 });
